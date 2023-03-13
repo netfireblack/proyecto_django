67e5c6208ee94e1dff4724cd4f978d2d499961b8
netfireblack <pablo.metalheart@gmail.com>
2023-03-13 17:34:23 -0300

    config-db

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'gmg0ixbvFLthZseqAYez',
+        'HOST': 'containers-us-west-205.railway.app',
+        'PORT': '6059'
     }
 }
 
